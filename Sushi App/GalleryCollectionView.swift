1a0a2242ef10d5e7d541d74e243e208a6f534b46
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-03-03 18:23:51 +0300

    GalleryCollectionView0.1

---
+++ Sushi App/GalleryCollectionView.swift
@@ -0,0 +1,21 @@
+//
+//  GalleryCollectionView.swift
+//  Sushi App
+//
+//  Created by Stepan Vasiljeu on 3/3/19.
+//  Copyright © 2019 Stepan Vasiljeu. All rights reserved.
+//
+
+import UIKit
+
+class GalleryCollectionView: UICollectionView {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
--- Sushi App/ViewController.swift
+++ Sushi App/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    private var galleryCollectionView = UICollectionView(frame: <#T##CGRect#>, collectionViewLayout: <#T##UICollectionViewLayout#>)
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
