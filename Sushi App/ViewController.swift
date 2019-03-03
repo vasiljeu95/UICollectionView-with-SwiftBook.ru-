5bbee4536d450c7100ad22a1641706b3b38cbf5a
Stepan Vasilyeu <38642038+vasiljeu95@users.noreply.github.com>
2019-03-03 18:07:02 +0300

    Initial Commit

---
+++ Sushi App/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Sushi App
+//
+//  Created by Stepan Vasiljeu on 3/3/19.
+//  Copyright © 2019 Stepan Vasiljeu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
